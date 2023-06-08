614d32b665699d22b277731f1705396cba1b4fc9
DarylT <daryl.tneoh@gmail.com>
2023-06-08 18:21:58 +0800

    fix(tool): move path to .env

--- global_data.py
+++ global_data.py
@@ -6,15 +6,13 @@ postmanApiKey = str(os.environ.get('POSTMAN_APIKEY'))
 collectionUid = os.environ.get('COLLECTION_UID')
 collectionName = str(os.environ.get('COLLECTION_NAME'))
 localUrl = os.environ.get('LOCAL_URL')
-localPort = os.environ.get('LOCAL_PORT')
 stagingUrl = os.environ.get('STAGING_URL')
-stagingPort = os.environ.get('STAGING_PORT')
 
 class Environ(Enum):
     local = 1
     staging = 2
 
 Path = {
-    1 : f'https://{localUrl}:{localPort}/swagger/v2/swagger.json',
-    2 : f'http://{stagingUrl}:{stagingPort}/swagger/v2/swagger.json'
+    1 : f'{localUrl}',
+    2 : f'{stagingUrl}'
 }
